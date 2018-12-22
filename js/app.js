3fb525a6e9a5b6f7d07c330affaf9111c9ab8721
Abdul Wahab <abwahab708@gmail.com>
2018-12-22 12:39:47 +0500

    getElementsByTagName
    getElementsByTagName returns a collection of elements. A collection is like in array you can access an element directly using its index or loop over the whole collection to access all of them.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
-/****************************
- * Simple Example 
-*/
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const color = document.getElementById('colorVal');
-const fontSize = document.getElementById('fontSize');
-const fontVal = document.getElementById('fontVal');
-const reset = document.getElementById('reset');
+const myList = document.getElementsByTagName('li');
+for (let index = 0; index < myList.length; index++) {
+  myList[index].style.color = 'red';
+}
 
-myButton.addEventListener('click', () => {
-  myHeading.style.color = color.value;
-});
-
-fontSize.addEventListener('click', () => {
-  myHeading.style.fontSize = `${fontVal.value}px`;
-});
-
-reset.addEventListener('click', () => {
-  myHeading.style.color = 'initial';
-  myHeading.style.fontSize = 'initial';
-});
+const myHeading = document.getElementsByTagName('h1')[0];
+myHeading.style.color = 'orange';
