eaa520a5989cac86132005076cd4b9721e20289a
Abdul Wahab <abwahab708@gmail.com>
2018-12-22 13:00:16 +0500

    getElementsByClassName
    Also returns an HTML collection

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ for (let index = 0; index < myList.length; index++) {
   myList[index].style.color = 'red';
 }
 
-// accessing the first h1 from the returned HTML collection
-const myHeading = document.getElementsByTagName('h1')[0];
-myHeading.style.color = 'orange';
+const specialItems = document.getElementsByClassName('special');
+// accessing all the individual items from the collection myList
+for (let index = 0; index < specialItems.length; index++) {
+  specialItems[index].style.color = 'purple';
+}
