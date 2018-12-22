04d5bfd24d0cfd34e6fdbd3ef1ff0c31d93deccd
Abdul Wahab <abwahab708@gmail.com>
2018-12-22 12:46:18 +0500

    Added a comment :+1:

--- js/app.js
+++ js/app.js
@@ -3,5 +3,6 @@ for (let index = 0; index < myList.length; index++) {
   myList[index].style.color = 'red';
 }
 
+// accessing the first h1 from the returned HTML collection
 const myHeading = document.getElementsByTagName('h1')[0];
 myHeading.style.color = 'orange';
