d5d5bc522af78853b66fefbda0bfcc5299da51b8
Abdul Wahab <abwahab708@gmail.com>
2018-12-22 15:09:53 +0500

    querySelector, querySelectorAll
    Query string must be a valid CSS selector string.
    Only difference between the two is querySelector will only return the first matching element, and querySelector All will return all elements that match the selector.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ for (let index = 0; index < myList.length; index++) {
   myList[index].style.color = 'red';
 }
 
-const specialItems = document.getElementsByClassName('special');
+const specialItems = document.querySelectorAll('.special');
 // accessing all the individual items from the collection myList
 for (let index = 0; index < specialItems.length; index++) {
   specialItems[index].style.color = 'purple';
+}
+
+const updateBackground = document.querySelectorAll('li:nth-child(odd)');
+for (let index = 0; index < updateBackground.length; index++) {
+  updateBackground[index].style.backgroundColor = 'gray';
 }
