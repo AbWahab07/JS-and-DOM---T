69371bf6a20db45a58f32a3b2fddad5714fc5670
Abdul Wahab <abwahab708@gmail.com>
2018-12-22 12:47:45 +0500

    Added another comment :+1:

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const myList = document.getElementsByTagName('li');
+// accessing all the individual items from the collection myList
 for (let index = 0; index < myList.length; index++) {
   myList[index].style.color = 'red';
 }
