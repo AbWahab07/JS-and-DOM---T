47fe43ed28a7ba49d427d30fcbebb021f7cf2d22
Abdul Wahab <abwahab708@gmail.com>
2018-12-23 10:23:04 +0500

    Style is an object on document(object).

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+const toggleList = document.querySelector('.toggleList');
+const listDiv = document.querySelector('.listDiv');
 const input = document.querySelector('input');
 const p = document.querySelector('p.description');
-const span = document.querySelector('span');
+const btn = document.querySelector('button.description');
 
-// Dynamically updating input type 
-input.type = 'checkbox';
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide List';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show List';
+    listDiv.style.display = 'none';
+  } 
+});
 
-// Dynamically setting the content of span
-span.innerHTML = `<code>${p.className}</code>`;
-
-// Dynamically adding the title to p
-p.title = 'Description of the list';
+btn.addEventListener('click', () => {
+  p.textContent = `${input.value}:`;
+});
